c087b8b3111aec13c2ba1ca42e007750676e97eb
Tyler Frailey <tfrailey1@gmail.com>
2026-09-15 14:05:59 -0400

    Basic Client-Server implementation

---
+++ Client.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    private Socket socket = null;
+    private BufferedReader input = null;
+    private DataOutputStream out = null;
+
+    public Client(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("Connected");
+
+            input = new BufferedReader(new InputStreamReader(System.in));
+
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException e) {
+            System.out.println("Could not find server");
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+
+        String line = "";
+
+        while (!line.equals("Over")) {
+            try {
+                line = input.readLine();
+                out.writeUTF(line);
+
+            } catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        try {
+            input.close();
+            out.close();
+            socket.close();
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+        new Client("127.0.0.1", 5000);
+    }
+}
